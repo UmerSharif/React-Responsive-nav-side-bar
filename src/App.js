62e96053cdcf5e32e24ddfe45fbaec11780d20a2
umer khan <umer.kdev@gmail.com>
2018-07-10 00:29:21 +0300

    adding side drawer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Toolbar from './component/Toolbar/Toolbar';
 import './index.css';
+import SideDrawer from './component/sidedrawer/SideDrawer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Toolbar />
+          <SideDrawer />
           <main>
               <p> This is the content area</p>
           </main>
---
+++ src/component/sidedrawer/SideDrawer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './sideDrawer.css';
+
+const SideDrawer = props => (
+    <nav className="side-drawer">
+        <ul>
+            <li><a href="/">Products</a></li>
+            <li><a href="/">User</a></li>
+        </ul>
+    </nav>
+
+);
+
+export default SideDrawer;
