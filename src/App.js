d5b5171bff299ffb82025a49243cdef4a11f0a56
umer khan <umer.kdev@gmail.com>
2018-07-10 21:28:32 +0300

    styling backdrop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Toolbar from './component/Toolbar/Toolbar';
 import './index.css';
 import SideDrawer from './component/sidedrawer/SideDrawer';
+import BackDrop from './component/backdrop/BackDrop';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
       <Toolbar />
           <SideDrawer />
+          <BackDrop />
           <main>
               <p> This is the content area</p>
           </main>
